0a9f02ef9eb58a9b8d280df67f0c4b0d91a05912
Sunaina Vallamkonda <svallamkonda@lcs.on.ca>
2019-11-18 11:16:27 -0500

    Initial Commit

---
+++ HappyOrSad/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HappyOrSad
+//
+//  Created by Vallamkonda, Sunaina on 2019-11-18.
+//  Copyright © 2019 Vallamkonda, Sunaina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
